ad538127e04c241b246aa3d01f8e16d9b159515a
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-05-30 21:06:14 -0400

    Finish project

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -1,20 +1,46 @@
 import { View, ScrollView, TouchableOpacity, Text, Image } from 'react-native'
 import NLWLogo from '../src/assets/logoSpaceTime.svg'
 import { Link, useRouter } from 'expo-router'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Icon from '@expo/vector-icons/Feather'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import * as SecureStore from 'expo-secure-store'
+import { api } from '../src/lib/api'
+import dayjs from 'dayjs'
+import ptBr from 'dayjs/locale/pt-br'
+
+dayjs.locale(ptBr)
+
+interface Memory {
+  coverUrl: string
+  excerpt: string
+  createdAt: string
+  id: string
+}
 
 export default function Memories() {
   const { bottom, top } = useSafeAreaInsets()
   const router = useRouter()
+  const [memories, setMemories] = useState<Memory[]>([])
 
   async function signOut() {
     await SecureStore.deleteItemAsync('token')
 
     router.push('/index')
   }
+  async function loadMemories() {
+    const token = await SecureStore.getItemAsync('token')
+
+    const response = await api.get('/memories', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    setMemories(response.data)
+  }
+  useEffect(() => {
+    loadMemories()
+  }, [])
 
   return (
     <ScrollView
@@ -39,37 +65,38 @@ export default function Memories() {
         </View>
       </View>
       <View className="mt-6 space-y-10">
-        <View className="space-y-4">
-          <View className="flex-row items-center gap-2">
-            <View className="h-px w-5 bg-gray-50" />
-            <Text className="font-body text-xs text-gray-100">
-              12 de Abril, 2023
-            </Text>
-          </View>
-          <View className="space-y-4 px-8">
-            <Image
-              source={{
-                uri: 'http://192.168.5.163:3333/uploads/c492f925-7747-4de0-8496-8fb22645a873.jpg',
-              }}
-              className="aspect-video w-full rounded-lg"
-              alt=""
-            />
-            <Text className="font-body text-base leading-relaxed text-gray-100">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod non
-              necessitatibus iure nobis, nulla esse aut temporibus, dolores
-              aperiam dolorem, dicta illo pariatur autem quam quidem recusandae
-              accusamus suscipit modi.
-            </Text>
-            <Link href="/memories/id" asChild>
-              <TouchableOpacity className="flex-row items-center gap-2">
-                <Text className="font-body text-sm text-gray-200">
-                  Ler mais
+        {memories.map((memory) => {
+          return (
+            <View key={memory.id} className="space-y-4">
+              <View className="flex-row items-center gap-2">
+                <View className="h-px w-5 bg-gray-50" />
+                <Text className="font-body text-xs text-gray-100">
+                  {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY ')}
                 </Text>
-                <Icon name="arrow-right" size={16} color="#9e9ea0" />
-              </TouchableOpacity>
-            </Link>
-          </View>
-        </View>
+              </View>
+              <View className="space-y-4 px-8">
+                <Image
+                  source={{
+                    uri: memory.coverUrl,
+                  }}
+                  className="aspect-video w-full rounded-lg"
+                  alt=""
+                />
+                <Text className="font-body text-base leading-relaxed text-gray-100">
+                  {memory.excerpt}
+                </Text>
+                <Link href="/memories/id" asChild>
+                  <TouchableOpacity className="flex-row items-center gap-2">
+                    <Text className="font-body text-sm text-gray-200">
+                      Ler mais
+                    </Text>
+                    <Icon name="arrow-right" size={16} color="#9e9ea0" />
+                  </TouchableOpacity>
+                </Link>
+              </View>
+            </View>
+          )
+        })}
       </View>
     </ScrollView>
   )
